c82e9c44b8abfe88bf657e1cd221b2b09328436e
David I <xizof@stuba.sk>
2023-04-29 15:04:59 +0200

    setup pins

--- final_zadanie.ino
+++ final_zadanie.ino
@@ -7,3 +7,21 @@ const int TIMER_ENABLED = false;
 const int buzzerPin = 9;
 char serialData;
 int inputPin = 10;
+
+dht DHT; 
+int readDHT, temp, hum;
+float speedOfSound, distance, duration;
+unsigned long previousMillis = 0;
+const long interval = 2000;
+bool beepSound;
+bool recordValues;
+
+void setup() {
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(inputPin, OUTPUT);
+  Serial.begin(9600);
+  beepSound = false;
+  recordValues = false;
+}
