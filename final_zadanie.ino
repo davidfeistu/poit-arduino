a2671da05eb98ffcc9f5b266ea5205e06600c855
David I <xizof@stuba.sk>
2023-04-29 15:04:07 +0200

    initialize port

---
+++ final_zadanie.ino
@@ -0,0 +1,9 @@
+#include <dht.h>
+#define dht11 8
+#define TRIGGER_PIN  12
+#define ECHO_PIN     11
+const int MAX_DISTANCE = 400;
+const int TIMER_ENABLED = false;
+const int buzzerPin = 9;
+char serialData;
+int inputPin = 10;
